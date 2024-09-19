a2d0b86d97c6f98d8448de4a9f33c8a7b48c27c8
Aref Hasni <arefhasni005@gmail.com>
2024-09-19 09:34:00 +0800

    Added custom footer JavaScript

---
+++ code-snippets/custom-footer.js
@@ -0,0 +1,6 @@
+document.aaddEventListener("DOMContentLoaded", function () {
+	const footer = document, createElement ("footer");>
+	footer.innerHTML = "<p> Custom footer added with collaboration</p>
+	document.body.appendChild(footer);
+});
+
